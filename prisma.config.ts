2ff6fa87ee8a371344ac478163aa8c22830a57dd
seunosinowo <oluwaseunpaul98@gmail.com>
2026-03-14 22:10:59 +0100

    update dependencies

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+  // migrations: {
+  //   path: "prisma/migrations",
+  // },
   datasource: {
     url: process.env["DATABASE_URL"],
   },
